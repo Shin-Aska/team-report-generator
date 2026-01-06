99b478e54e406e7644f29edd72bb6a311275c6da
Richard Louie Orilla <richard.orilla@inriver.com>
2026-01-06 20:36:30 +0800

    feat: add support for trusted proxies in application configuration

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,20 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $trusted = array_filter(array_map(
+            'trim',
+            explode(',', (string) env('TRUSTED_PROXIES', ''))
+        ));
+
+        if ($trusted) {
+            $middleware->trustProxies(
+                at: $trusted,
+                headers: Request::HEADER_X_FORWARDED_FOR
+                    | Request::HEADER_X_FORWARDED_HOST
+                    | Request::HEADER_X_FORWARDED_PORT
+                    | Request::HEADER_X_FORWARDED_PROTO
+            );
+        }
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
